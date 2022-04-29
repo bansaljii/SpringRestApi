c80ff2f23295db6371a8655da4766fab04d4dbfa
Abhishek Bansal <bansalabhi143368@gmail.com>
2022-04-29 22:34:44 +0530

    second commit

--- src/main/java/com/rakuten/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.rakuten.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rakuten.Entities.Course;
-
-public interface CourseDao extends JpaRepository<Course, Long>{
-
-}
--- src/main/java/com/rakuten/services/CourseService.java
+++ src/main/java/com/rakuten/services/CourseService.java
@@ -2,18 +2,12 @@ package com.rakuten.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.rakuten.Entities.Course;
-
 public interface CourseService {
 
 	public Course getCourse(long courseId);
-
 	public List<Course> getCourses();
-
 	public Course addCourse(Course course);
 	public Course updateCourse(Course course);
-
 	public Course deleteCourse(long courseId);
 }
